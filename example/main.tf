05be2440d52cee206c220c443d83a6d4756ec6ef
Matthew Lehr <gitops@fwd.lunardrift.net>
2026-09-01 18:24:10 -0400

    fmt

--- example/main.tf
+++ example/main.tf
@@ -2,7 +2,7 @@ module "x_dns" { # ld_dns
   source = "../"
   domain_name = "webdatabasesolutions.com"
   dns_providers = ["aws", "dnsimple"]
-  default_ttl = 60
+  default_ttl   = 60
 
   txt_records = {
     "@" = [
@@ -12,10 +12,17 @@ module "x_dns" { # ld_dns
     "_dmarc" = ["v=DMARC1; p=quarantine; pct=100; adkim=s; aspf=s"]
   }
   cname_records = {
-    "dkim._domainkey" = "dkim._domainkey.alias.proton.me"
+    "dkim._domainkey"   = "dkim._domainkey.alias.proton.me"
     "dkim02._domainkey" = "dkim02._domainkey.alias.proton.me"
     "dkim03._domainkey" = "dkim03._domainkey.alias.proton.me"
   }
+  # TODO implement MX records
+  # mx_records = {
+  #   "@" = [
+  #     "10 mx1.alias.proton.me",
+  #     "20 mx2.alias.proton.me"
+  #   ]
+  # }
 }
 
 
